84e83295217a85546625947d0d175ea71cf9eb9a
charlie <charlesgold45@gmail.com>
2023-10-05 23:24:18 +0000

    redux

---
+++ src/store/slice/authSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+        isLoggedIn: true
+    },
+    reducers: {
+        TOGGLE_AUTH: (state) => {
+            state.isLoggedIn = !state.isLoggedIn;
+        }
+        
+    }
+})
+
+export const { TOGGLE_AUTH } = authSlice.actions;
+
+export default authSlice.reducer;
---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import countReducer from "./slice/countSlice";
+import authReducer from "./slice/authSlice";
+
+// create a store
+const store = configureStore({
+  reducer: {
+    counter: countReducer,
+    auth: authReducer,
+  },
+});
+
+export default store;
